a40fbf3c894b9c30e2fc005ed01e801969678a68
deekshith024 <deekshithreddy0001@gmail.com>
2022-05-27 22:20:16 -0400

    s commit

--- FA_Deekshith_C0854956_iOS/ViewController.swift
+++ FA_Deekshith_C0854956_iOS/ViewController.swift
@@ -9,8 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-   
+   var player = 1
+    var state = [0,0,0,0,0,0,0,0,0]
+    var stateIsActive = true
     
+    @IBAction func Buttons(_ sender: UIButton)
+    {
+        if (state[sender.tag-1] == 0 && stateIsActive == true)
+        {
+            state[sender.tag-1] = player
+            if ( player == 1)
+            {
+                sender.setImage(UIImage(named: "cross.png"), for: UIControl.State())
+                player = 2
+            }
+            else
+            {
+                sender.setImage(UIImage(named: "nought.png"), for: UIControl.State())
+                player = 1
+            }
+        }
+        
+        
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
